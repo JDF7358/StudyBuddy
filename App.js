d4a4ee8cffc30c7ea149a1dd22cd384fb3873e5a
thekellychen <kell31ch@gmail.com>
2018-01-25 22:46:41 -0500

    Login screen has username and password text input

--- App.js
+++ App.js
@@ -1,81 +1,85 @@
 import React from 'react';
-import { AppRegistry, StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 class HomeScreen extends React.Component {
-	static navigationOptions = {
-		title: 'StudyBuddy',
-	};
-	render() {
-		const { navigate } = this.props.navigation;
-		return (
-			<View style={styles.container}>
-				<Button
-					onPress={() => navigate('Login')}
-					title="Login"
-				/>
-				<Button
-					onPress={() => navigate('Register')}
-					title="Register"
-				/>
-			</View>
-		);
-	}
+  static navigationOptions = {
+    title: 'StudyBuddy',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Button
+          onPress={() => navigate('Login')}
+          title="Login" />
+        <Button
+          onPress={() => navigate('Register')}
+          title="Register" />
+      </View>
+    );  
+  }
 }
 
 // if local: force push
 
 class LoginScreen extends React.Component {
-	static navigationOptions = {
-		title: 'Login',
-	};
-	render() {
-		return (
-      <View style={styles.container}>
-        <Text>This is the 'Login' screen.</Text>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here"
-        />
-      </View>
-    );
-	}
+  static navigationOptions = {
+    title: 'Login',
+  };
+  
+  constructor(props) {
+        super(props);
+        this.state = { text: '' };
+    }
+
+    render() {
+        return (
+          <View style = {{padding: 20}}>
+          <TextInput
+            placeholder = {'Username'}
+              style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+              clearTextOnFocus = {true}/>
+
+            <TextInput
+              placeholder = {'Password'}
+              style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+              clearTextOnFocus = {true}/>
+            </View>
+        );
+    }
 }
 
 class RegisterScreen extends React.Component {
-	static navigationOptions = {
-		title: 'Register',
-	};
-	render() {
-		return (
-      <View style={styles.container}>
-      <Text>This is the 'Register' screen.</Text>
-      <TextInput
-        style={{height: 40}}
-        placeholder="Type here"
-      />
-      </View>
-    );
-	}
+  static navigationOptions = {
+    title: 'Register',
+  };
+  render() {
+    return <Text>This is the 'Register' screen.</Text>;
+  }
 }
 
 export const StudyBuddy = StackNavigator({
-	Home: { screen: HomeScreen },
-	Login: { screen: LoginScreen },
-	Register: { screen: RegisterScreen },
+  Home: { screen: HomeScreen },
+  Login: { screen: LoginScreen },
+  Register: { screen: RegisterScreen },
 });
 
 export default class App extends React.Component {
-	render() {
-		return <StudyBuddy />;
-	}
+  render() {
+    return <StudyBuddy />;
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'white',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
