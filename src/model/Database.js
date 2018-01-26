fc4d0cb5a995b0d4d27d8956b6b65c70be30ff15
Tristen Allen <spyboticsguy@gmail.com>
2018-01-26 11:12:57 -0500

    Add temporary constructor code to drop the users DB when our DB singleton is created

--- src/model/Database.js
+++ src/model/Database.js
@@ -4,9 +4,13 @@ class Database {
   static users_table = 'users';
 	constructor(name) {
 		this.db = SQLite.openDatabase(name);
+    // TEMPORARY: drop users table before creating.
+    this.db.transaxtion(tx => {
+      tx.executeSql('drop table ?', [this.users_table]);
+    });
     this.db.transaction(tx => {
       tx.executeSql('create table if not exists ? (id integer primary key not null, '
-        + 'firstName text, lastName text, password text, username text);', [this.users_table]);
+        + 'firstName text, lastName text, password text, username text, major text, year text);', [this.users_table]);
     });
   }
 
@@ -15,9 +19,11 @@ class Database {
    */
   addUser(lastName, firstName, email, year, major, password) {
     this.db.transaction(tx => {
-      tx.executeSql('insert into ? (firstName, lastName, password, username) VALUES (?, ?, ?, ?)');
+      tx.executeSql('insert into ? (firstName, lastName, password, username, major, year) VALUES (?, ?, ?, ?, ?, ?)',
+        [this.users_table, firstName, lastName, password, email, major, year]);
     });
   }
+
 }
 
 export const StudyBuddyDB = new Database('studybuddy.db');
