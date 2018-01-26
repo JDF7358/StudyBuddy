26b40725010244b1a1fdbceea24df8063aeaa257
Nihjur Yarbrough <niyarbro@hotmail.com>
2018-01-25 21:51:00 -0500

    Home screen, login screen, and register screen navigation.

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+	static navigationOptions = {
+		title: 'StudyBuddy',
+	};
+	render() {
+		const { navigate } = this.props.navigation;
+		return (
+			<View>
+				<Button
+					onPress={() => navigate('Login')}
+					title="Login"
+				/>
+				<Button
+					onPress={() => navigate('Register')}
+					title="Register"
+				/>
+			</View>
+		);	
+	}
+}
+
+// if local: force push
+
+class LoginScreen extends React.Component {
+	static navigationOptions = {
+		title: 'Login',
+	};
+	render() {
+		return <Text>This is the 'Login' screen.</Text>;
+	}
+}
+
+class RegisterScreen extends React.Component {
+	static navigationOptions = {
+		title: 'Register',
+	};
+	render() {
+		return <Text>This is the 'Register' screen.</Text>;
+	}
+}
+
+export const StudyBuddy = StackNavigator({
+	Home: { screen: HomeScreen },
+	Login: { screen: LoginScreen },
+	Register: { screen: RegisterScreen },
+});
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	render() {
+		return <StudyBuddy />;
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'green',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
