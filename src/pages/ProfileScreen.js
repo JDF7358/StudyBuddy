5812d85fd413fd0141170d1473c163f14771dbc2
Nihjur Yarbrough <niyarbro@hotmail.com>
2018-02-11 19:25:20 -0500

    ProfileScreen header looks better.

--- src/pages/ProfileScreen.js
+++ src/pages/ProfileScreen.js
@@ -7,10 +7,13 @@ import UserProfilePageCmp from '../components/UserProfilePageCmp.js';
 export default class ProfileScreen extends React.Component {
   
   static navigationOptions = ({navigation}) => ({
+	  headerTitleStyle: {textAlign: 'center', alignSelf: 'center'},
 	  title: "My Profile",
-	  headerLeft: <TouchableOpacity onPress = {() => navigation.navigate('SidebarMenu')}>
+	  headerLeft: <TouchableOpacity onPress = {() => navigation.navigate('SidebarMenu')}
+	    style = {{margin: 10, padding: 10}}>
 		  <Text>Menu</Text></TouchableOpacity>,
-	  headerRight: <TouchableOpacity onPress = {() => navigation.navigate('EditMyProfile')}>
+	  headerRight: <TouchableOpacity onPress = {() => navigation.navigate('EditMyProfile')}
+	    style = {{margin: 10, padding: 10}}>
 		  <Text>Edit</Text></TouchableOpacity>
   });
   
