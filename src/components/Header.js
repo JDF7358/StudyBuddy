5c4e9da7d87c5f80754d9bc335e1d2aa7a222222
Kelly Chen <kell31ch@gmail.com>
2018-04-19 23:42:59 -0400

    changed appearance of search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { View, TextInput } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 import Styles from '../components/Styles.js';
 
 const Header = () => (
-  <View style={Styles.Hcontainer}>
-    <TextInput
-      style={Styles.Hinput}
-      placeholder="Search..."
-      onChangeText={(text) => console.log('searching for ', text)}
-    />
-  </View>
+  //<View style={Styles.Hcontainer}>
+    // <TextInput
+    //   style={Styles.Hinput}
+    //   placeholder="Search..."
+    //   onChangeText={(text) => console.log('searching for ', text)}
+    // />
+    <SearchBar
+     	lightTheme
+    	onChangeText={(text) => console.log('searching for', text)}
+    	placeholder="Search" /> 
+  //</View>
 );
 
 export default Header;
